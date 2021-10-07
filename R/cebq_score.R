6eb28d202327cd01da873522d2fdb71d1b9710d3
ALAINA L PEARCE <gu.alaina.pearce@gmail.com>
2021-10-07 13:42:01 -0400

    start v1 child script

---
+++ R/cebq_score.R
@@ -0,0 +1,112 @@
+#' cebq_score: Calculates the Children's Eating Behavior Questionnaire sub-scales for PROPERLY formatted data
+#'
+#' This function calculates the the eight Children's Eating Behavior Questionnaire subscales:
+#'
+#' @param cebq_dat A dataset with the 35 Children's Eating Behavior Questionnaire questions and raw responses with each row representing a participant
+#' @param parID_var variable name for column containg participant IDs, if cebq_dat contains IDS
+#' @param reverse_scored (logic) TRUE/FLASE - raw responses have already been reversed scored (e.g., due to how the levels were coded in RedCap)
+#'
+#' @return A numeric value for each of the eight subscales for each row in the dataset
+#'
+#' @examples
+#'
+#' \dontrun{
+#' }
+#'
+#' @seealso To get best fit parameters use \code{\link{LODE_Fit}}.
+#' To get fit your intake data using the  the Quadratic model
+#' (Kissileff, 1982; Kissileff & Guss, 2001), see \code{\link{Quad_Fit}}
+#' and \code{\link{Quad_n2ll}}.
+#'
+#' @export
+LODE_n2ll <- function(cebq_dat, parID_var, reverse_scored) {
+
+  ####             1. Set up/initial checks             #####
+  # make sure parameters are numeric
+  if (is.character(par[[1]])) {
+    par <- as.numeric(par)
+  } else if (is.data.frame(par)) {
+    par <- data.matrix(par)
+  }
+
+
+  # get entered of default function names as characters
+  if (model_str == 'LODE' | model_str == 'lode'){
+    #set to standard str
+    model_str <- 'LODE'
+
+    #get functions
+    time_fn <- substitute(LODE_Time)
+    fit_fn <- substitute(LODE_Fit)
+    intake_fn <- substitute(LODE_Intake)
+  } else if (model_str == 'Quad' | model_str == 'quad'){
+    #set to standard str
+    model_str <- 'Quad'
+
+    #get functions
+    time_fn <- substitute(Quad_Time)
+    fit_fn <- substitute(Quad_Fit)
+    intake_fn <- substitute(Quad_Intake)
+  } else {
+    stop("model_str does not match available models. Options are 'LODE' or 'Quad'")
+  }
+
+  #check error input args
+  error_arg <- methods::hasArg(error_method)
+  error_measure_arg <- methods::hasArg(error_measure)
+  if (isTRUE(error_arg)) {
+    if (error_method == 'rmse' | error_method == 'RMSE'){
+      #set standard str
+      error_method <- 'rmse'
+    } else if (error_method == 'R2' | error_method == 'r2'){
+      #set standard str
+      error_method <- 'R2'
+    } else if (error_method == 'both' | error_method == 'Both'){
+      #set standard str
+      error_method <- 'both'
+    } else {
+      stop("Error was not calculated: Unrecognized value for error_method. Options include: 'rmse', 'R2', or 'both'.")
+    }
+
+    #add measure names
+    if (isTRUE(error_measure_arg)) {
+      if (error_measure == "timing" | error_measure == "Timing") {
+        #set standard str
+        error_measure <- 'timing'
+      } else if (error_measure == "intake" | error_measure == "Intake") {
+        #set standard str
+        error_measure <- 'intake'
+      } else if (error_measure == 'both' | error_measure == 'Both'){
+        #set standard str
+        error_measure <- 'both'
+      } else {
+        stop("Error was not calculated: Unrecognized value for error_measure. Options include: 'timing', 'intake', or 'both'.")
+      }
+    } else {
+      #default is timing
+      error_measure <- 'timing'
+    }
+  }
+
+  #check data input
+  data_arg <- methods::hasArg(data)
+
+  # get estimated intake
+  data$Estimated_intake <- sapply(data[, timeVar], LODE_Intake, parameters = c(par[1], par[2]), Emax = Emax)
+
+  # re-name estimated intake variable
+  estimated_name <- paste0("Estimated_", intakeVar)
+  names(data)[length(names(data))] <- estimated_name
+
+  # calculate the error/residual between predicted and actual intake
+  data$resid <- data[, intakeVar] - data[, estimated_name]
+
+  # get sigma
+  sigma <- sum(data$resid^2) / length(data$resid)
+
+  # ll equation
+  ll <- (-length(data$resid) / 2) * (log(2 * pi * sigma^2)) + ((-1 / (2 * sigma^2)) * (sum(data$resid^2)))
+
+  # rerun -2ll
+  return(-2 * ll)
+}
---
+++ R/qualtrics_child_v1dat.R
@@ -0,0 +1,88 @@
+#' qualtrics_child_v1dat: Process raw qualtrics visit 1 data for the child
+#'
+#' This function ...
+#'
+#' @param date_str
+#' @param data_path (optional)
+#'
+#'
+#' @return A raw database containing child visit 1 data from qualtrics
+#'
+#' @examples
+#'
+#' \dontrun{
+#' }
+#'
+#' @seealso To get best fit parameters use \code{\link{LODE_Fit}}.
+#' To get fit your intake data using the  the Quadratic model
+#' (Kissileff, 1982; Kissileff & Guss, 2001), see \code{\link{Quad_Fit}}
+#' and \code{\link{Quad_n2ll}}.
+#'
+#' @export
+qualtrics_child_v1dat <- function(date_str, data_path) {
+
+
+  # while teseting only
+  data_path <- '/Users/azp271/OneDrive - The Pennsylvania State University/b-childfoodlab_Shared/Active_Studies/RO1_Brain_Mechanisms_IRB_5357/Participant_Data/untouchedRaw/Qualtrics_Raw/'
+  date_str <- '2021_09_16'
+
+  ####             1. Set up/initial checks             #####
+  # check that date_str exist and is a string
+  datastr_arg <- methods::hasArg(date_str)
+
+  if (isTRUE(datastr_arg) & !is.character(date_str)) {
+    stop("data_str must be enter as a string: e.g., '2021_09_16'")
+  } else if (isFALSE(datastr_arg)) {
+    stop("data_str must set to the data string from the child visit 1 file name: e.g., '2021_09_16'")
+  }
+
+  # check that file exists
+  datapath_arg <- methods::hasArg(data_path)
+
+  if (isTRUE(datapath_arg)){
+    if (!is.character(date_str)){
+      stop("data_path must be enter as a string: e.g., '.../Participant_Data/untouchedRaw/Qualtrics_Raw/'")
+    } else {
+      qv1_child_path <- base::paste0(data_path, '/', 'Child_V1_', date_str, '.sav')
+      qv1_child_exists <- base::file.exists(qv1_child_path)
+    }
+
+  } else {
+    qv1_child_exists <- base::file.exists(qv1_child_path)
+  }
+
+  #load data if it exists
+  if (isTRUE(qv1_child_exists)){
+    qv1_child_dat <- as.data.frame(haven::read_spss(qv1_child_path))
+
+  } else {
+    if (isTRUE(datapath_arg)){
+      stop('File does not exist. Check date_str and data_path entered')
+    } else {
+      stop('File does not exist. Check date_str and that the data exists in current working directory')
+    }
+  }
+
+
+  ####             2. Clean Data             #####
+
+  # extract variable labels
+  qv1_child_labels <- lapply(qv1_child_dat, function(x) attributes(x)$label)
+
+  # clean the data
+  qv1_child_clean <- qv1_child_dat[c(1, 11:13, 20:39, 44:46, 55:81, 92:103, 112, 121, 130,
+                                   139, 148, 157:335)]
+
+
+  # re-label and organize
+  ## order ideas: 1) child information (sex, dob, h/w, bmi, screen out), freddies, food VAS, intakes (preMeal, EAH, meal duration), wanting, PSD, PSS, notes/etc
+
+  ## get start date in correct format
+
+  ## re-name variables
+
+  ## check labels to be sure all good (most shouldn't need to change)
+
+  ## id all factors and change label values to start at 0 (e.g. change sex from 1-?, 2-? to 0-?, 1-?)
+  ## probs can do with lapply statement
+}
